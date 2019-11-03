41853f2b6b592deb722255ea9ebcc84ccb212e7e
syeda zaidi <zaidisyeda03@gmail.com>
2019-11-02 21:02:22 -0700

    controller with db queries added

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -21,5 +21,35 @@ module.exports = {
             console.log(err);
         })
     },
+
+    create: function( req, res) {
+        db.Book.create(req.body)
+        .then( dbbooks => {
+            res.json(dbbooks);
+        }).catch (err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+    },
+
+    update: function( req, res) {
+        db.Book.update( { _id: req.params.id }, req.body)
+        .then( dbbooks => {
+            res.json(dbbooks);
+        }).catch (err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+    },
+
+    remove: function (req, res) {
+        db.Book.deleteOne({ _id: req.params.id })
+        .then( dbbooks => {
+            res.json(dbbooks);
+        }).catch (err => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+    }
     
-}
+};
