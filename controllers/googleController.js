d26df2a8a083f7439dace08a4d3af16a5b453df7
syeda zaidi <zaidisyeda03@gmail.com>
2019-11-03 12:41:41 -0800

    controller for google books api

---
+++ controllers/googleController.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+module.exports = {
+
+    findGoogleBooks: function (req, res) {
+        let search = req.params.query;
+        let url = "https://www.googleapis.com/books/v1/volumes?q=" + search + "&key=" + process.env.APIKEY;
+        console.log (url);
+
+        axios.get(url).then( result => {
+            res.json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+        })
+    },
+};
