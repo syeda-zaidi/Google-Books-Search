9a4f7d68da1d88d6f5154267a1242a435740b478
syeda zaidi <zaidisyeda03@gmail.com>
2019-11-02 20:54:35 -0700

    nav component

---
+++ client/src/components/nav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <div className="navbar navbar-dark bg-dark">
+            <div className="navbar-brand"> Google Books </div>
+            <div className="items"> Search </div>
+            <div className="items"> Saved </div>
+        </div>
+    )
+}
+
+export default Nav;
